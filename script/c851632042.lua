7cddd48d3d5b582eaa453c954cb37642f72601ae
Fran Luque <franluque2@gmail.com>
2024-09-19 18:30:45 -0300

    Made Charmethyst OPD

--- script/c851632042.lua
+++ script/c851632042.lua
@@ -3,14 +3,14 @@ Duel.LoadScript ("wb_aux.lua")
 local s,id=GetID()
 function s.initial_effect(c)
 
-    --add
-    local e1=Effect.CreateEffect(c)
+	--add
+	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
 	e1:SetProperty(EFFECT_FLAG_DELAY)
-	e1:SetCountLimit(1,id)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_DUEL)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1,false)
@@ -18,7 +18,7 @@ function s.initial_effect(c)
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
 	c:RegisterEffect(e2)
 	
-    aux.GlobalCheck(s,function()
+	aux.GlobalCheck(s,function()
 		local ge=Effect.CreateEffect(c)
 		ge:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 		ge:SetCode(EVENT_STARTUP)
@@ -29,14 +29,14 @@ end
 
 function s.setup(e,tp,eg,ep,ev,re,r,rp)
 	for i = 0, 1, 1 do
-        local totalmons=Duel.GetMatchingGroupCount(Card.IsType, i, LOCATION_DECK, 0, nil, TYPE_MONSTER)
-        local totalspells=Duel.GetMatchingGroupCount(Card.IsType, i, LOCATION_DECK, 0, nil, TYPE_SPELL)
-        local totaltraps=Duel.GetMatchingGroupCount(Card.IsType, i, LOCATION_DECK, 0, nil, TYPE_TRAP)
+		local totalmons=Duel.GetMatchingGroupCount(Card.IsType, i, LOCATION_DECK, 0, nil, TYPE_MONSTER)
+		local totalspells=Duel.GetMatchingGroupCount(Card.IsType, i, LOCATION_DECK, 0, nil, TYPE_SPELL)
+		local totaltraps=Duel.GetMatchingGroupCount(Card.IsType, i, LOCATION_DECK, 0, nil, TYPE_TRAP)
 
-        if totalmons==totalspells and totalmons==totaltraps then
-            Duel.RegisterFlagEffect(i, id, 0, 0, 0)
-        end
-    end
+		if totalmons==totalspells and totalmons==totaltraps then
+			Duel.RegisterFlagEffect(i, id, 0, 0, 0)
+		end
+	end
 	e:Reset()
 end
 
@@ -52,15 +52,15 @@ function s.trapfilter(c)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetFlagEffect(tp, id)>0 and Duel.IsExistingMatchingCard(s.monfilter,tp,LOCATION_DECK,0,1,nil) 
-        and Duel.IsExistingMatchingCard(s.spelfilter,tp,LOCATION_DECK,0,1,nil) and Duel.IsExistingMatchingCard(s.trapfilter,tp,LOCATION_DECK,0,1,nil)
-     end
+		and Duel.IsExistingMatchingCard(s.spelfilter,tp,LOCATION_DECK,0,1,nil) and Duel.IsExistingMatchingCard(s.trapfilter,tp,LOCATION_DECK,0,1,nil)
+	 end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,3,tp,LOCATION_DECK)
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
-    Duel.ShuffleDeck(tp)
-    local monster=Duel.GetFirstMatchingCard(s.monfilter, tp, LOCATION_DECK, 0, nil)
-    local spell=Duel.GetFirstMatchingCard(s.spelfilter, tp, LOCATION_DECK, 0, nil)
-    local trap=Duel.GetFirstMatchingCard(s.trapfilter, tp, LOCATION_DECK, 0, nil)
+	Duel.ShuffleDeck(tp)
+	local monster=Duel.GetFirstMatchingCard(s.monfilter, tp, LOCATION_DECK, 0, nil)
+	local spell=Duel.GetFirstMatchingCard(s.spelfilter, tp, LOCATION_DECK, 0, nil)
+	local trap=Duel.GetFirstMatchingCard(s.trapfilter, tp, LOCATION_DECK, 0, nil)
 
 	local g=Group.FromCards(monster,spell,trap)
 
